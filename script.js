50b5bd60a31283c0f91d4d53db274819be918c8f
AM_00 <taole.chen@gmail.com>
2023-01-03 23:23:44 +0100

    skeleton set up and basic elements added.

---
+++ script.js
@@ -0,0 +1,14 @@
+class Project {
+    
+    constructor(title){
+        this.title = title;
+
+    }
+}
+
+
+const body = document.querySelector('body');
+const projectsContainer = document.querySelector('#projectsContainer');
+body.addEventListener('scroll', () => {
+    
+})
