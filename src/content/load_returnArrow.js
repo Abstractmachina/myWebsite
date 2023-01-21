262813980b2bc25ac43fec85d502281969def417
AM_00 <taole.chen@gmail.com>
2023-01-21 23:22:37 +0100

    return arrow for profile button added

---
+++ src/content/load_returnArrow.js
@@ -0,0 +1,11 @@
+
+
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="960px"
+	 height="560px" viewBox="0 0 960 560" enable-background="new 0 0 960 560" xml:space="preserve">
+    <g id="line">
+	    <path fill="#FFFFFF" stroke="#000000" stroke-miterlimit="10" d="M22,29.5h-9.5c-6.627,0-12-5.373-12-12V0"/>
+    </g>
+    <g id="head">
+        <circle cx="29.875" cy="29.125" r="7.875"/>
+    </g>
+</svg>
