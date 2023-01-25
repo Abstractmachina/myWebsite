ae229e7569055b0b5a689d5f83ce510c1608be41
AM_00 <taole.chen@gmail.com>
2023-01-25 18:50:25 +0100

    aag updated

--- src/projects/proj_aag.js
+++ src/projects/proj_aag.js
@@ -1,6 +1,14 @@
 import { Project } from "../Project";
-import { createElementText } from "../util";
-import aag_cover from '../assets/aag/aag_cover.jpg'
+import { createElementText, createFigure} from "../util";
+
+//media
+import aag_cover from '../assets/aag/aag_cover.jpg';
+import processDiagram from '../assets/aag/diagram_process.png';
+import testPrints from '../assets/aag/tests.jpg';
+import ws0 from '../assets/aag/workshop00.jpg';
+import ws1 from '../assets/aag/workshop01.jpg';
+import exhibit0 from '../assets/aag/exhibition00.jpg';
+import exhibit1 from '../assets/aag/exhibition01.jpg';
 
 const aag = () => {
     let proj = new Project(
@@ -26,6 +34,11 @@ const aag = () => {
     intro1.textContent = "Formwork-Free and Mortar-Free Shell Assembly is a workshop I led in Gothenburg, Sweden, for the Advances in Architectural Geometry Conference in 2018.";
     const intro2 = document.createElement("p");
     intro2.textContent = "The objective was to demonstrate to students the workings and benefits of the computational design paradigm that allows seamless integration between design, fabrication and assembly in settings where many complex constraints come together, thus making it unsuitable for conventional, analog design methodologies. Pedagogically, the students experienced it firsthand by designing funicular shell structures that hold together without formwork assembly and mortar through a parametric model that embeds fabrication data, subsequently sending their design elements to a robotic arm for hotwire fabrication. This results in a tight feedback loop where students were able to see the effects of their design decisions in a real-world prototype with a minimal delay and adapt their design in the next iteration."; 
+
+    const vid_fab = document.createElement('div');
+    vid_fab.classList.add('projectVideoContainer');
+    vid_fab.innerHTML = '<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/792684368?h=bf02f19b1f&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="AAG - Shells - Fabrication Clip"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>';
+
     //Features
     const featuresHeading = createElementText("h2", 'Features');
     const features = document.createElement("ul");
@@ -43,8 +56,16 @@ const aag = () => {
     const tech4 = createElementText('li', 'Half-edge mesh structure to faciliate geometric operations and force simulation');
     tech.append(tech1, tech2, tech3,tech4);
 
-    proj.pushContents(ti, img_cover, introheading, intro1, intro2, 
-        featuresHeading, features, techHeading, tech);
+
+    const img_process = createFigure(processDiagram, 'Process diagram');
+    const img_tests = createFigure(testPrints, "Small scale test prints of different interlock designs")
+    const img_ws0 = createFigure(ws0, "Class tutorials to introduce tools.")
+    const img_ws1 = createFigure(ws1, "Robot workshop for the students")
+    const img_exhibit0 = createFigure(exhibit0, "Our spot in the final exhibition")
+    const img_exhibit1 = createFigure(exhibit1, "Final exhibition space")
+
+    proj.pushContents(ti, img_cover, introheading, intro1, img_ws0, img_ws1, intro2, vid_fab, img_exhibit0, img_exhibit1,
+        featuresHeading, features, img_process, img_tests, techHeading, tech);
 
     return proj;
 }
